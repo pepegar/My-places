1e4a892414ee3bbdbac454760db5e06387209f6c
José Luis García <jl.garhdez@gmail.com>
2013-01-14 22:21:23 +0100

    a

--- src/me/jlgarcia/mislugares/ListaLugaresActivity.java
+++ src/me/jlgarcia/mislugares/ListaLugaresActivity.java
@@ -1,20 +1,46 @@
 package me.jlgarcia.mislugares;
 
+import me.jlgarcia.mislugares.db.LugaresSQLHelper;
 import android.os.Bundle;
-import android.provider.Contacts.People;
-import android.widget.SimpleCursorAdapter;
-import android.app.ListActivity;
-import android.widget.ListAdapter;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 import android.app.Activity;
-import android.database.Cursor;
+import android.app.ListActivity;
 
-public class ListaLugaresActivity extends ListActivity {
+public class ListaLugaresActivity extends Activity 
+{
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lista_lugares);
+	
+		ListView lista = (ListView) findViewById(R.id.listaLugares);
 		
-		
+		LugaresSQLHelper dbHelper = new LugaresSQLHelper(this);
+
+//		dbHelper.insertarLugar("Nombre del primer elemento", "Descripcion del primer elemento", (long) 44.276671, (long) -106.888183, "no-foto");
+//		dbHelper.insertarLugar("Nombre del segundo elemento", "Descripcion del segundo elemento", (long) 40.276671, (long) -110.888183, "no-foto");
+	
+		String[] lugares = dbHelper.leerNombresDeLugares();
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, lugares);
+		lista.setAdapter(adapter);
+
+		// Listener de cada uno de los elementos del listview
+		lista.setOnItemClickListener(new OnItemClickListener(){
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View view, int position, long id) {
+			    Toast.makeText(getApplicationContext(), "El contenido de este item es: " + position, Toast.LENGTH_LONG).show();
+			}
+			
+		});
 		
 	}
+
 }
--- src/me/jlgarcia/mislugares/db/LugaresSQLHelper.java
+++ src/me/jlgarcia/mislugares/db/LugaresSQLHelper.java
@@ -2,29 +2,33 @@ package me.jlgarcia.mislugares.db;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class LugaresSQLHelper extends SQLiteOpenHelper
 {
+	
 	public LugaresSQLHelper(Context context ) 
 	{
+		
 		super(context, LugaresDB.DB_NAME, null, LugaresDB.DB_VERSION);
+		
 	}
 	
 	@Override
 	public void onCreate(SQLiteDatabase db) 
 	{
+
 		if(db.isReadOnly()){db = getWritableDatabase();}
 		db.execSQL("CREATE TABLE " +
 		LugaresDB.Lugares.NOMBRE_TABLA + "(" +
-		LugaresDB.Lugares._ID + " INTEGER PRIMARY KEY AUTO INCREMENT, " +
+		LugaresDB.Lugares._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
 		LugaresDB.Lugares.NOMBRE + " VARCHAR(40), " +
 		LugaresDB.Lugares.DESCRIPCION + " TEXT, " +
 		LugaresDB.Lugares.LATITUD + " REAL, " +
 		LugaresDB.Lugares.LONGITUD + " REAL, " +
 		LugaresDB.Lugares.FOTO + " TEXT) ");
-		
 		
 	}
 	
@@ -54,5 +58,50 @@ public class LugaresSQLHelper extends SQLiteOpenHelper
 		valores.put("foto", foto);
 		
 		this.getWritableDatabase().insert(LugaresDB.Lugares.NOMBRE_TABLA, null, valores);
+	}
+	
+	public String[] leerNombresDeLugares()
+	{
+		
+		String ret[];
+		
+		String[] columnas = {"_ID", "nombre", "descripcion", "latitud", "longitud", "foto"};
+		
+		Cursor c = this.getReadableDatabase().query(LugaresDB.Lugares.NOMBRE_TABLA, columnas, null, null, null, null, null);
+		
+		int id, nombre, descripcion, latitud, longitud, foto;
+		id = c.getColumnIndex("_ID");
+		nombre = c.getColumnIndex("nombre");
+		descripcion = c.getColumnIndex("descripcion");
+		latitud = c.getColumnIndex("latitud");
+		longitud = c.getColumnIndex("longitud");
+		foto = c.getColumnIndex("foto");
+	
+		ret = new String[c.getCount()];
+	
+		int i = 0;
+		// Recorremos el contenido del cursor
+		for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext())
+		{
+			ret[i] = c.getString(nombre);
+			i++;
+		}
+		
+		return ret;
+	
+	}
+	
+	public String[] getLugarByName(String name){
+		
+		SQLiteDatabase db = getWritableDatabase();
+		
+		String[] ret = new String[6];
+		
+		String[] args = new String[]{name};
+		
+		Cursor c = db.query(false, LugaresDB.Lugares.NOMBRE_TABLA, args, "nombre=?", args, null, null, null, null);
+		
+		return ret;
+		
 	}
 }
